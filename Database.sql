d7e853f020823e0f8f3692b0c4bea020b8257802
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-02-24 20:27:50 +0200

    [0.00.001] Add failures database table (SQL).

---
+++ Database.sql
@@ -0,0 +1,10 @@
+create table euphoria_failures
+(
+    key_failure bigint unsigned auto_increment,
+    title varchar(100) not null,
+    summary text not null,
+    data json not null,
+    constraint euphoria_failures_pk
+        primary key (key_failure)
+);
+
