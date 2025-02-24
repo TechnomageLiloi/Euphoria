09a82de34a537acca0e0eb3c108185ec6f42b78f
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-02-24 20:45:27 +0200

    [0.00.003] Add failures domain.

--- Database.sql
+++ Database.sql
@@ -2,6 +2,7 @@ create table euphoria_failures
 (
     key_failure bigint unsigned auto_increment,
     title varchar(100) not null,
+    dt timestamp not null,
     summary text not null,
     data json not null,
     constraint euphoria_failures_pk
---
+++ Domains/Failures/Manager.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Liloi\Euphoria\Domains\Failures;
+
+use Liloi\Euphoria\Domains\Manager as DomainManager;
+
+class Manager extends DomainManager
+{
+    /**
+     * Get table name.
+     *
+     * @return string
+     */
+    public static function getTableName(): string
+    {
+        return self::getTablePrefix() . 'road';
+    }
+
+    public static function loadCollection(): Collection
+    {
+        $name = self::getTableName();
+
+        $rows = self::getAdapter()->getArray(sprintf(
+            'select * from %s order by key_failure desc;',
+            $name
+        ));
+
+        $collection = new Collection();
+
+        foreach($rows as $row)
+        {
+            $collection[] = Entity::create($row);
+        }
+
+        return $collection;
+    }
+
+    /**
+     * Load problem from database.
+     *
+     * @param string $keyRoad
+     * @return Entity
+     */
+    public static function load(string $keyRoad): Entity
+    {
+        $name = self::getTableName();
+
+        $row = self::getAdapter()->getRow(sprintf(
+            'select * from %s where key_failure="%s"',
+            $name,
+            $keyRoad
+        ));
+
+        return Entity::create($row);
+    }
+
+    /**
+     * Save problem to database.
+     *
+     * @param Entity $entity
+     */
+    public static function save(Entity $entity): void
+    {
+        $name = self::getTableName();
+        $data = $entity->get();
+
+        $RID = $data['key_failure'];
+
+        self::update(
+            $name,
+            $data,
+            sprintf('key_failure = "%s"', $RID)
+        );
+    }
+
+    /**
+     * Create problem in database.
+     */
+    public static function create(): Entity
+    {
+        $key = date('Y-m-d H:i:s');
+
+        $name = self::getTableName();
+        $data = [
+            'key_failure' => $key,
+            'title' => 'Enter failure title',
+            'dt' => date('Y-m-d H:i:s'),
+            'summary' => '-',
+            'data' => '{}'
+        ];
+
+        self::insert($name, $data);
+        return Entity::create($data);
+    }
+}
